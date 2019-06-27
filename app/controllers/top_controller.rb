7171b582441166e69b34486a52e02d3c1b5856b0
takashi-kozuka <takashi.kozuka@medpeer.co.jp>
2019-06-27 17:30:43 +0900

    トップに人気のタグを表示

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -3,5 +3,6 @@ class TopController < ApplicationController
     @recent_users = User.recent.with_attached_images.limit(6)
     @this_month_users = User.this_month.with_attached_images
     @tags_histories = UserTagHistory.add.includes(:user, :user_tag).recent.limit(10)
+    @popular_tags = UserTag.popular.limit(20)
   end
 end
