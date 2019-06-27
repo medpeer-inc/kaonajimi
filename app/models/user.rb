0bcac4f71ceaf180a870c392a187d51ba4dd01fc
takashi-kozuka <takashi.kozuka@medpeer.co.jp>
2019-06-27 11:59:15 +0900

    デザインあてる（途中)

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  belongs_to :master_group, class_name: 'Master::Group'
+  belongs_to :master_group, class_name: 'Master::Group', foreign_key: :master_group_id, inverse_of: :users
   has_many :user_jobs, dependent: :destroy
   has_many :master_jobs, class_name: 'Master::Job', through: :user_jobs
 
