00ca0a3cba44ac87d68e425d707d7b6bc05c4e5f
Madogiwa <17684091+Madogiwa0124@users.noreply.github.com>
2019-06-27 17:38:53 +0900

    fix :bug:

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,7 +1,7 @@
 class TopController < ApplicationController
   def index
-    @recent_users = User.recent.with_attached_images.limit(6)
-    @this_month_users = User.this_month.with_attached_images
+    @recent_users = User.recent.with_attached_sub_images.limit(6)
+    @this_month_users = User.this_month.with_attached_sub_images
     @tags_histories = UserTagHistory.add.includes(:user, :user_tag).recent.limit(10)
   end
 end
