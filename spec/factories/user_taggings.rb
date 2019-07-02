d3a0a278ec0ceb7367f8bf2ef679a7b81de8b104
arihh <arihh0615@gmail.com>
2019-07-02 22:35:46 +0900

    create factories

---
+++ spec/factories/user_taggings.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user_tagging do
+    user
+    user_tag
+  end
+end
---
+++ spec/factories/user_tags.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :user_tag do
+    title { "酒好き" }
+    description { "酒を愛しているものたち" }
+  end
+end
